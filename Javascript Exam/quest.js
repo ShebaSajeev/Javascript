ed57f5fd4594a2ee2b5142fa7a6920a3844d1623
ShebaSajeev <163380206+ShebaSajeev@users.noreply.github.com>
2024-08-31 13:45:15 +0530

    Delete Javascript Exam directory

--- Javascript Exam/quest.js
+++
@@ -1,8 +0,0 @@
-let cname = data.name;
-            let currtemp = kelvinToCelsius(data.main.temp);
-            let weatherDesc = data.weather[0].description;
-            let hume = data.main.humidity;
-            let pres = data.main.pressure;
-            let windspeed = data.wind.speed;
-            console.log(windspeed);
-            <i class="fa-solid fa-gauge text-center me-3"></i>
